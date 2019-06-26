ae638cfb0dd6db4635f3dd719a029579c5b17507
xjq946 <xjq111006@163.com>
2019-06-27 00:04:25 +0800

    Initial commit

---
+++ src/main/java/com/example/testconflict/TestconflictApplication.java
@@ -0,0 +1,13 @@
+package com.example.testconflict;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestconflictApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestconflictApplication.class, args);
+    }
+
+}
